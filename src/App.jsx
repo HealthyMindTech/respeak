6a0fc3ee0ac92a0c1292bc7b0cb004528087367d
Troels Nielsen <bn.troels@gmail.com>
2022-03-25 23:47:38 +0100

    First draft of though writing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Navbar, ThemeProvider, Row, Col, Container } from 'react-bootstrap';
+import { Button, Navbar, Form, ThemeProvider, Row, Col, Container } from 'react-bootstrap';
 
 function App() {
   return (
@@ -12,7 +12,17 @@ function App() {
       <Container>
         <Row>
           <Col xs={3} />
-          <Col xs={6}>Hello there</Col>
+          <Col xs={6}>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Please write down your thoughts: </Form.Label>
+                <Form.Control as="textarea" rows={5} />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Pass thoughts to the great void
+              </Button>
+            </Form>
+          </Col>
           <Col xs={3} />
         </Row>
       </Container>
