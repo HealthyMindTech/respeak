cd8fbd9ad460e1ce481965329e70f6261ae0857e
petcupaula <petcu.paula@gmail.com>
2022-03-25 23:52:24 +0100

    Add help text and another thoughts form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,65 @@
-import { Button, Navbar, Form, ThemeProvider, Row, Col, Container } from 'react-bootstrap';
+import React, { useState } from "react";
+import {Navbar, ThemeProvider, Row, Col, Container, Modal, Button, Form} from 'react-bootstrap';
+
+function InfoDialog() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="outline" onClick={handleShow}>
+        ?
+      </Button>
+
+      <Modal show={show} onHide={handleClose} scrollable="true">
+        <Modal.Header closeButton>
+          <Modal.Title>Respeak</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Share your thoughts and help others reframe theirs.</p>
+          <p>Write down what thoughts are burdening you at the moment, and click SEND. </p>
+          <p>Afterward, help others reframe their thoughts. Can you find a different 
+          perspective for their challenge? </p>
+          <p>For every 2 thoughts that you help reframe, you get one of your thoughts reframed. 
+          And you can inspect the history of all the thoughts.</p>
+          <hr></hr>
+          <p>Example</p>
+          <p>Thought:</p>
+          <p><i>“I failed the math test today and I feel useless and so stupid.”</i></p>
+          <p>Try to rephrase this. For example:</p>
+          <p><i>“I have not prepared enough for the math test. I have gone through many tests in my 
+            life and succeeded. Next time I can prepare better. ”</i></p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+function ThoughtsForm() {
+  return(
+    <Form>
+    <Form.Group className="mb-3" controlId="formBasicThought">
+      <Form.Label>What's on your mind?</Form.Label>
+      <Form.Control as="textarea" placeholder="Enter thoughts here" rows="3" />
+      <Form.Text className="text-muted">
+        Hint: Just share what's burdening you atm. 
+      </Form.Text>
+    </Form.Group>
+    <Button variant="primary" type="submit">
+      SEND
+    </Button>
+  </Form>
+);
+}
+
 
 function App() {
   return (
@@ -7,12 +68,14 @@ function App() {
       <Navbar bg="light">
         <Container>
           <Navbar.Brand href="#home">Respeak</Navbar.Brand>
+          <InfoDialog />
         </Container>
       </Navbar>
       <Container>
         <Row>
           <Col xs={3} />
           <Col xs={6}>
+          <ThoughtsForm/>
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Please write down your thoughts: </Form.Label>
