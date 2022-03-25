d3791d44ead6b83bdeebbb935854bf72abb5ae86
Troels Nielsen <bn.troels@gmail.com>
2022-03-25 23:11:26 +0100

    Use jsx extension where it makes sense

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ThemeProvider from 'react-bootstrap/ThemeProvider';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <div className="App">
+        
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
